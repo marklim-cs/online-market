5c5cac8cd9d661c2bc7ea3dd4212489ad6202dd9
Marina <climovicimarina@gmail.com>
2025-03-23 17:42:43 +0100

    products serializers

--- onlinemarket/products/models.py
+++ onlinemarket/products/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
 
---
+++ onlinemarket/products/serializers.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+from rest_framework import serializers
+from .models import Category, Product
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["name", "description"]
+
+class ProductSerializer(serializers.ModelSerializer):
+    price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    class Meta:
+        model = Product
+        fields = ["name", "description", "price", "price_after_tax", ]
+        depth = 1 #or category = CategorySerializer()
+
+    def calculate_tax(self, product: Product):
+        return product.price*Decimal(1.1)
