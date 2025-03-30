cf2fc09a3080c7af5f5b7197f160effa5861f6ef
Marina <climovicimarina@gmail.com>
2025-03-30 15:45:34 +0200

    filtering products by query params

--- onlinemarket/products/urls.py
+++ onlinemarket/products/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import list_views, cart_views
 
 urlpatterns = [
+    path('', list_views.ProductList.as_view(), name='product_'),
     path('categories/', list_views.CategoryList.as_view(), name='categories'),
-    path('categories/<slug:category_slug>/', list_views.ProductList.as_view(), name='product-list'),
+    path('categories/<slug:category_slug>/', list_views.ProductList.as_view(), name='category-product-list'),
     path('cart/', cart_views.CartView.as_view(), name='cart'),
 ]
--- onlinemarket/products/views/list_views.py
+++ onlinemarket/products/views/list_views.py
@@ -4,20 +4,32 @@ from rest_framework.response import Response
 from rest_framework import status
 from products.models import Category, Product
 from products.serializers import CategorySerializer, ProductSerializer
-
+from products.services import filter_products_by_query_params
 
 class CategoryList(APIView):
     def get(self, request):
         categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True, context={'request': request})
-        return Response(serializer.data)
+        category_serializer = CategorySerializer(categories, many=True)
+
+        products = filter_products_by_query_params(request)
+        if products:
+            product_serializer = ProductSerializer(products, many=True)
+            return Response(product_serializer.data)
+
+        return Response(category_serializer.data)
 
 class ProductList(APIView):
     serializer_class = ProductSerializer
 
     def get(self, request, category_slug):
         category = get_object_or_404(Category, slug=category_slug)
+        category_name = category.name
         products = Product.objects.filter(category=category)
+
+        filtered_products = filter_products_by_query_params(request, category=category_name)
+        if filtered_products:
+            products = filtered_products
+
         serializer = self.serializer_class(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
